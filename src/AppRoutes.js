842bb6a7871ef31dd7b8a3e4fc6cdb92499d4ed4
nguyenduchoaang <duchoaang.dev@gmail.com>
2024-10-04 00:41:41 +0700

    init commit

---
+++ src/AppRoutes.js
@@ -0,0 +1,17 @@
+import React, { Suspense } from 'react';
+import Loading from './based/Loading';
+
+const HomePage = React.lazy(() => import('./homepage/homepage'));
+
+const AppRoutes = [
+   {
+      path: '/',
+      element: (
+         <Suspense fallback={<Loading isOpen={true} />}>
+            <HomePage />
+         </Suspense>
+      ),
+   },
+];
+
+export default AppRoutes;
